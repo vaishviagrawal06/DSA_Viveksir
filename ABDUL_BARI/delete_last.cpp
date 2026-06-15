3e1f463efbeaef0d48267e4998c642d70421b132
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-06-15 21:22:06 +0530

    delete last node

---
+++ ABDUL_BARI/delete_last.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+node *first, *temp, *ttemp, *pre, *curr, *nextt, *p;
+
+void init()
+{
+    first = temp = ttemp = NULL;
+}
+void createfirst(int x)
+{
+    first = new node;
+    first->data = x;
+    first->next = NULL;
+}
+
+void addNode(int val)
+{
+    temp = first;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    ttemp = new node;
+    ttemp->data = val;
+    ttemp->next = NULL;
+    temp->next = ttemp;
+}
+
+void display()
+{
+    temp = first;
+    while (temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+void delete_last()
+{
+    temp = first;
+    while (temp->next != NULL)
+    {
+        ttemp = temp;
+        temp = temp->next;
+    }
+    ttemp->next = NULL;
+    delete temp;
+}
+
+int main()
+{
+    init();
+    createfirst(10);
+    addNode(20);
+    addNode(30);
+    addNode(40);
+    cout << "before deleting: " << endl;
+    display();
+    delete_last();
+    cout << "after deleting: " << endl;
+    display();
+}
