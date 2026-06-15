5e162faa91d5b4f6f1d9fc858e545db8b87a34c4
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-06-15 21:47:40 +0530

    delete after a given data using SLL:

---
+++ ABDUL_BARI/delete_aftergiven_data.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+node *first, *temp, *ttemp, *p;
+
+void init()
+{
+    first = temp = ttemp = NULL;
+}
+void createfirst(int x)
+{
+    first = new node;
+    first->data = x;
+    first->next = NULL;
+}
+
+void addNode(int val)
+{
+    temp = first;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    ttemp = new node;
+    ttemp->data = val;
+    ttemp->next = NULL;
+    temp->next = ttemp;
+}
+
+void display()
+{
+    temp = first;
+    while (temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+void del_aftergiven_data(int x)
+{
+    temp = first;
+    while (temp->data != x)
+    {
+        temp = temp->next;
+    }
+    ttemp = temp->next;
+    p = ttemp->next;
+    temp->next = p;
+    ttemp->next = NULL;
+    delete ttemp;
+}
+
+int main()
+{
+    init();
+    createfirst(10);
+    addNode(20);
+    addNode(30);
+    addNode(40);
+    addNode(50);
+    cout << "before deleting: " << endl;
+    display();
+    del_aftergiven_data(20);
+    cout << "deleting after a given data: " << endl;
+    display();
+}
