6fe34093bc2e2c51e319fd5b9ece21157221b395
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-06-15 20:50:06 +0530

    add before a given data using singe linked list

---
+++ ABDUL_BARI/add_beforegiven_data.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+node *first, *temp, *ttemp, *pre, *curr, *nextt ,*p;
+
+void init()
+{
+    first = temp = ttemp = NULL;
+}
+void createfirst(int x)
+{
+    first = new node;
+    first->data = x;
+    first->next = NULL;
+}
+
+void addNode(int val)
+{
+    temp = first;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    ttemp = new node;
+    ttemp->data = val;
+    ttemp->next = NULL;
+    temp->next = ttemp;
+}
+
+void display()
+{
+    temp = first;
+    while (temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+void add_beforegiven_data(int x,int y)
+{
+    temp=first;
+    while(temp->next->data!=x)
+    {
+        ttemp=temp->next;
+        temp=temp->next;
+    }
+    p=new node;
+    p->data=y;
+    p->next=ttemp;
+    temp->next=p;
+}
+int main()
+{
+    init();
+    createfirst(10);
+    addNode(20);
+    addNode(30);
+    cout<<"befor adding: "<<endl;
+    display();
+    add_beforegiven_data(20,45);
+    cout<<"after adding: "<<endl;
+    display();
+}
